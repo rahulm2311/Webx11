308af6c1a725e65aeb9639d62e86589541ce7f23
rahulm2311 <103928016+rahulm2311@users.noreply.github.com>
2022-04-18 16:57:00 +0530

    System
    i am Done

---
+++ MT15/src/login/Password.java
@@ -0,0 +1,11 @@
+package login;
+
+public class Password {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Password is Entered");
+
+	}
+
+}
---
+++ MT15/src/login/Username.java
@@ -0,0 +1,10 @@
+package login;
+
+public class Username {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("User name is Entered");
+	}
+
+}
